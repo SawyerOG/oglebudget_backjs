3bed4221121988f291e06a91274adca0f8e3dfdd
SawyerOG <oglesbysaw@gmail.com>
2021-09-18 14:13:34 -0600

    .....

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const { createPool } = require('mysql2');
+
+const dev = false;
+
 const pool = createPool({
-	// connectionLimit: 4,
-	host: process.env.dbHost,
-	user: process.env.dbUser,
-	password: process.env.dbPassword,
-	database: process.env.dbDatabase,
-	waitForConnections: true,
-	multipleStatements: true,
+    host: dev ? process.env.dbHostdev : process.env.dbHostprod,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    database: process.env.dbDatabase,
+    waitForConnections: true,
+    multipleStatements: true,
 });
 
 exports.db = pool.promise();
